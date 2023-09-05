0f318ccad1f1fdcc7510155a73043bc59e38d113
Gabriel Ferreira <gabrielcsf@gmail.com>
2023-09-05 17:17:24 -0300

    Creating store item resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StoreModule } from './store/store.module';
+import { ItemModule } from './item/item.module';
 
 @Module({
-  imports: [StoreModule],
+  imports: [StoreModule, ItemModule],
   controllers: [AppController],
   providers: [AppService],
 })
