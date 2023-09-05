a0202206cc4d397395e6ba19895c33fe5c3429a9
Gabriel Ferreira <gabrielcsf@gmail.com>
2023-09-05 17:18:36 -0300

    Creating sale resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StoreModule } from './store/store.module';
 import { ItemModule } from './item/item.module';
+import { SaleModule } from './sale/sale.module';
 
 @Module({
-  imports: [StoreModule, ItemModule],
+  imports: [StoreModule, ItemModule, SaleModule],
   controllers: [AppController],
   providers: [AppService],
 })
