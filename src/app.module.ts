c7c0c72baad17e41564fcbaf01669be94a6789c5
Gabriel Ferreira <gabrielcsf@gmail.com>
2023-09-06 12:06:49 -0300

    Adding configuration for Nest and reading env variables from config file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StoreModule } from './store/store.module';
 import { ItemModule } from './item/item.module';
 import { SaleModule } from './sale/sale.module';
 
+import configuration from '../config/configuration';
+
+
+let modelModules = [StoreModule, ItemModule, SaleModule];
+
 @Module({
-  imports: [StoreModule, ItemModule, SaleModule],
+  imports:
+    [ConfigModule.forRoot({
+      load: [configuration],
+      envFilePath: ['.env.development.local', '.env.development'],
+    }),
+    ...modelModules],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule { }
