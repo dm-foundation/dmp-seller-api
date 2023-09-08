a1304e16eeac7e2c9c63b7cd5fa2fc276c4dcb2c
Felipe Almeida <felipealmeidadevelop@gmail.com>
2023-09-08 11:03:20 -0300

    feat: create entities

--- src/sale/entities/sale.entity.ts
+++ src/sale/entities/sale.entity.ts
@@ -1,1 +1,52 @@
-export class Sale {}
+import { Item } from 'src/item/entities/item.entity';
+import { Store } from 'src/store/entities/store.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export enum Status {
+  PENDING = 'pending',
+  COMPLETE = 'complete',
+  INVOICE_CREATED = 'invoice_created',
+  INVOICE_SENT = 'invoice_sent',
+}
+
+@Entity('sales')
+export class Sale {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column()
+  customer_name: string;
+
+  @Column()
+  customer_email: string;
+
+  @Column({
+    type: 'enum',
+    enum: Status,
+    default: Status.PENDING,
+  })
+  stauts: Status;
+
+  @ManyToOne(() => Store)
+  @JoinColumn({ name: 'id_store' })
+  store: Store;
+
+  @OneToMany(() => Item, (item) => item.id)
+  @JoinColumn({ name: 'id_item' })
+  items: Item[];
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
