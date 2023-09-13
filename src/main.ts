026bc59e15a60e274d0bd08dddf3bd4a21df7ef9
Gabriel Ferreira <gabrielcsf@gmail.com>
2023-09-13 17:47:27 -0300

    Adding CORS config to application

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-
+let cors = require("cors");
 async function bootstrap() {
+
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const corsOptions = {
+    origin: 'http://localhost:3001',
+    credentials: true,
+    optionSuccessStatus: 200
+  }
+  app.use(cors(corsOptions));
+
   await app.listen(3000);
 }
 bootstrap();
+
