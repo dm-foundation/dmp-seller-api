959fe31b5a70c3a6d89e7ce49a590ea0cddbdd21
Gabriel Ferreira <gabrielcsf@gmail.com>
2023-09-26 20:05:05 -0300

    Adding status to Sale DTO; Adding localhost to CORS list again

--- src/sale/dto/create-sale.dto.ts
+++ src/sale/dto/create-sale.dto.ts
@@ -1,7 +1,9 @@
 import { Item } from "src/item/entities/item.entity";
+import { Status } from "../entities/sale.entity";
 
 export class CreateSaleDto {
     customer_email: string;
+    status: Status;
     storeId: number;
     amountInUSD: number;
     amountInEth: number;
